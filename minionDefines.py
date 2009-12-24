29e3484767c8d26140ddcd08b03f2116287885c0
David Brown <508374+ckode@users.noreply.github.com>
2009-12-24 19:29:57 +0000

    chat server works now! and ansi color defines

--- minionDefines.py
+++ minionDefines.py
@@ -1,4 +1,50 @@
 # Game Status defines
 LOGOUT           = 0
 LOGIN            = 1
-EDIT             = 2
+GETNAME          = 2
+PLAYING          = 3
+GETPASSWORD      = 4
+NEW              = 5
+EDIT             = 50
+
+
+#ANSI
+
+RESET              = chr(27) + "[0m"
+BOLD               = chr(27) + "[1m"
+ITALIC             = chr(27) + "[3m"
+UNDERLINE          = chr(27) + "[4m"
+INVERSE            = chr(27) + "[7m"
+STRIKE             = chr(27) + "[9m"
+BOLD_OFF           = chr(27) + "[22m"
+ITALIC_OFF         = chr(27) + "[23m"
+UNDERLINE_OFF      = chr(27) + "[24m"
+INVERSE_OFF        = chr(27) + "[27m"
+STRIKE_OFF         = chr(27) + "[29m"
+BLACK              = chr(27) + "[30m"
+RED                = chr(27) + "[31m"
+GREEN              = chr(27) + "[32m"
+YELLOW             = chr(27) + "[33m"
+BLUE               = chr(27) + "[34m"
+MAGENTA            = chr(27) + "[35m"
+CYAN               = chr(27) + "[36m"
+WHITE              = chr(27) + "[37m"
+
+LRED                = chr(27) + "[1;31m"
+LGREEN              = chr(27) + "[1;32m"
+LYELLOW             = chr(27) + "[1;33m"
+LBLUE               = chr(27) + "[1;34m"
+LMAGENTA            = chr(27) + "[1;35m"
+LCYAN               = chr(27) + "[1;36m"
+
+DEFAULT            = chr(27) + "[39m"
+B_BLACK            = chr(27) + "[40m"
+B_RED              = chr(27) + "[41m"
+B_GREEN            = chr(27) + "[42m"
+B_YELLOW           = chr(27) + "[43m"
+B_BLUE             = chr(27) + "[44m"
+B_MAGENTA          = chr(27) + "[45m"
+B_CYAN             = chr(27) + "[46m"
+B_WHITE            = chr(27) + "[47m"
+B_DEFAULT          = chr(27) + "[49m"
+
