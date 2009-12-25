315cfc98d1e9a76604c9aeb7f3ec2a2739180eb2
ckode <ckode@727538f7-4bcb-4e44-8fbd-912d6da47b83>
2009-12-25 02:34:33 +0000

    * Added filter for nonprintable characters * add who command (though need to make them more than if-else) * fixed some of the commands for sending text out to client

--- minionsParser.py
+++ minionsParser.py
@@ -7,6 +7,9 @@ import re, string
 
 
 def commandParser(player, line):
+    # Remove all unprintable characters except BACKSPACE
+    line = filter(lambda x: x in minionDefines.PRINTABLE_CHARS, line)
+
     # Player isn't logged in yet, do dialog
     if player.STATUS != minionDefines.PLAYING:
         NotPlayingDialog(player, line)
@@ -18,21 +21,33 @@ def commandParser(player, line):
     elif cmd[0].lower() == "/quit":
         player.disconnectClient()
     elif cmd[0].lower() == "users":
-        player.sendLine(str(len(player.factory.players)))
+        player.sendLine(str(len(player.factory.players)) + " players connected.\r\n")
+    elif cmd[0].lower() == "gos":
+         cmdLen = len(cmd[0]) + 1
+         player.Shout(minionDefines.MAGENTA + player.name + " gossips: " + line[cmdLen:] + minionDefines.WHITE )
+    elif cmd[0].lower() == "who":
+        player.sendToPlayer(minionDefines.CYAN + "<<=-=-=-=-= Whos Online =-=-=-=>>")
+        for user in player.factory.players:
+            player.sendToPlayer(minionDefines.CYAN + "  => " + minionDefines.MAGENTA + user.name)
+        player.sendToPlayer(minionDefines.CYAN + "<<=-=-=-=-=-=-=-=-=-=-=-=-=-=-=>>")
     else:
-        player.sendToRoom(line)
+        player.sendToRoom(minionDefines.GREEN + player.name + " says: " +minionDefines.WHITE + line)
+        player.sendLine(minionDefines.GREEN + "You say: " + minionDefines.WHITE + line)
 
-        
+
 def NotPlayingDialog(player, line):
-    
+
     if player.STATUS == minionDefines.GETNAME:
-        player.name = line
+        line = line.split()
+        name = line[0]
+        player.name = name.capitalize()
         player.STATUS = minionDefines.GETPASSWORD
         player.transport.write("Enter your password: ")
         return
     elif player.STATUS == minionDefines.GETPASSWORD:
+        player.Shout(player.name + " has joined.")
         player.STATUS = minionDefines.PLAYING
-        player.transport.write(minionDefines.LYELLOW + "Welcome " + player.name + "\r\n")
-        player.factory.sendMessageToAllClients(player.name + " has joined.")
+        player.sendToPlayer(minionDefines.LYELLOW + "Welcome " + player.name + "\r\n" )
+
         return
 
