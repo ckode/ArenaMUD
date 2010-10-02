f4b2551658c477d9cc526ffcb5c7e71f4e4c22d5
ckode <ckode@727538f7-4bcb-4e44-8fbd-912d6da47b83>
2010-10-02 22:10:50 +0000

    * forgot to add data/rcdata.sql (class and race data)

---
+++ data/rcdata.sql
@@ -0,0 +1,10 @@
+INSERT INTO race (id, name, description, BaseHP, DamageBonus, CastingBonus, Vision, DefenseBonus, ToHitBonus, Stealth) VALUES (1, "Human", "A human", 100, 0, 0, 1, 0, 0, 0);
+INSERT INTO race (id, name, description, BaseHP, DamageBonus, CastingBonus, Vision, DefenseBonus, ToHitBonus, Stealth) VALUES (2, "Dark-Elf", "A Dark-Elf", 90, 0, 20, 2, 5, 5, 30);
+INSERT INTO race (id, name, description, BaseHP, DamageBonus, CastingBonus, Vision, DefenseBonus, ToHitBonus, Stealth) VALUES (3, "Ogre", "A Ogre", 120, 5, -25, 2, -5, -5, -30);
+INSERT INTO race (id, name, description, BaseHP, DamageBonus, CastingBonus, Vision, DefenseBonus, ToHitBonus, Stealth) VALUES (4, "Goblin", "A Goblin", 80, 0, 10, 2, 10, 10, 30);
+
+
+INSERT INTO class (id, name, description, HPBonus, MinDamage, MaxDamage, BaseArmor, MageryType, Stealth, WeaponMessages) VALUES (1, "Barbarian", "A barbarian", 15, 5, 15, 50, 1, 0, 1);
+INSERT INTO class (id, name, description, HPBonus, MinDamage, MaxDamage, BaseArmor, MageryType, Stealth, WeaponMessages) VALUES (2, "Mage", "A Mage", 0, 10, 20, 50, 2, 0, 2);
+INSERT INTO class (id, name, description, HPBonus, MinDamage, MaxDamage, BaseArmor, MageryType, Stealth, WeaponMessages) VALUES (3, "Theif", "A Thief", 0, 5, 15, 50, 0, 50, 3);
+INSERT INTO class (id, name, description, HPBonus, MinDamage, MaxDamage, BaseArmor, MageryType, Stealth, WeaponMessages) VALUES (4, "Priest", "A Priest", 0, 10, 20, 50, 3, 0, 4);
