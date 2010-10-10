89074a9003f801230a0e56d054ca4930d5ce63b5
David Brown <508374+ckode@users.noreply.github.com>
2010-10-10 04:14:09 +0000

    * Fix bug if you try to bash where a door doesn't exist. * Added countlines.cmd file that will count lines of code that exist. (.py files)

---
+++ countlines.cmd
@@ -0,0 +1,10 @@
+@ECHO OFF
+type amCommands.py | find /C /V "#"
+type amDB.py | find /C /V "#"
+type amDefines.py | find /C /V "#"
+type amLog.py | find /C /V "#"
+type amParser.py | find /C /V "#"
+type amRace.py | find /C /V "#"
+type amRooms.py | find /C /V "#"
+type amUtils.py | find /C /V "#"
+type ArenaMUD.py | find /C /V "#"
