04355befedeaad3076dea76bf51bcd5a7ffc5c5c
ckode <ckode@727538f7-4bcb-4e44-8fbd-912d6da47b83>
2010-11-20 17:14:26 +0000

    * Updated spellsetup.sql schema for some changes * adding playerspells.sql which is data for spellsetup.sql

--- data/spellsetup.sql
+++ data/spellsetup.sql
@@ -20,10 +20,10 @@ CREATE TABLE spells_items (
       name              VARCHAR(25) NOT NULL,      -- Item or spell name
       cmd               VARCHAR(4) NOT NULL,       -- Command used to evoke
       casted            INTEGER NOT NULL,          -- Is it picked up or casted (picked up 0, casted 1)
-      use               INTEGER NOT NULL,          -- 1 self, 2 both, 3 other person
+      use               INTEGER NOT NULL,          -- 1 self, 2 both, 3 other person, 4 area effect
       reqClass          INTEGER NOT NULL,          -- class IDs 
       duration          INTEGER NOT NULL,          -- How long it lasts 0 = instant like a single heal (if duration > 0 effects can go above max stat)
-      durationEffect    INTEGER NOT NULL,
+      durationEffect    INTEGER NOT NULL,          -- If an effect is applied each duration segment like damage, or healing.
       effects           VARCHAR(50) NOT NULL,      -- Stored as "stat: value" say HP = 1 "1: 20" would add 20HP
       guesture          VARCHAR(100) NOT NULL,     -- if item, %s picks up, if spell, makes guesture. separate self / room with "|" (or use "*" to ignore)
       effectText        VARCAHR(100) NOT NULL,     -- Text seen when duration effect happens. self only. (you feel better single heal, or You feel sick fo poison etc
